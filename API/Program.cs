402343dd3c86b2c71495bee49139b240a932a3cd
Alberto Fonseca Quintanell <alberto.fonsecaq@gmail.com>
2026-04-12 12:48:32 -0400

    End section 5

--- API/Program.cs
+++ API/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
+    .AddJwtBearer(options => 
     {
         var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("Token key not found - Program.cs");
         options.TokenValidationParameters = new TokenValidationParameters
@@ -27,7 +27,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
             ValidateIssuer = false,
             ValidateAudience = false
-        };
+        }; 
     });
 
 var app = builder.Build();
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,20 +1,23 @@
 import {Component, inject, OnInit, signal} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import {lastValueFrom} from 'rxjs';
+import {Nav} from '../layout/nav/nav';
+import {AccountService} from '../core/services/account-service';
+import {Home} from '../features/home/home';
+import {User} from '../types/user';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Nav,Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
-
+  private accountService = inject(AccountService)
   private http = inject(HttpClient);
   protected readonly title = signal<string>('Dating App');
-  protected members = signal<any>([]);
+  protected members = signal< User[] >([]);
 
   async ngOnInit() {
     /**this.http.get('https://localhost:5001/api/members').subscribe({
@@ -24,11 +27,19 @@ export class App implements OnInit {
 
     })*/
     this.members.set(await this.getMembers())
+    this.setCurrentUser();
   }
+
+  setCurrentUser() {
+     const userString = localStorage.getItem('user');
+     if(!userString) return;
+     const user = JSON.parse(userString);
+     this.accountService.currentUser.set(user);
+   }
 
   async getMembers() {
     try {
-      return lastValueFrom(this.http.get('https://localhost:5001/api/members'))
+      return lastValueFrom(this.http.get< User[] >('https://localhost:5001/api/members'))
     }
     catch (error) {
       console.log(error)
